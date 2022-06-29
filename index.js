61ab50fab68d56f94270217a5dcc438acc9d9cdd
Takuya Tsuchiya <takuya21@gmail.com>
2022-06-29 14:16:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 console.log("hello world!こんにちは、Ether.jsの勉強をしています");
 console.log("こんにちは、Ether.jsの勉強をしています");
 
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
 
-<p id="demo"></p>
