ef9ce86663e8a1195d6be2ebdf0dffc15861df8a
Takuya Tsuchiya <takuya21@gmail.com>
2022-06-30 14:00:44 +0900

    blick numberをawaitしてからalert

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 console.log("hello world!");
 console.log("こんにちは、Ether.jsの勉強をしています");
 
-const provider = new ethers.providers.Web3Provider(window.ethereum)
+async function myFunction() {
+  
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
 
-await provider.send("eth_requestAccounts", []);
-
-const signer = provider.getSigner()
-
-  a = provider.getBlockNumber();
-  console.log(a);
-
+  const blocknumber = await provider.getBlockNumber();
+  
+  alert(blocknumber);
+}  
 
 
