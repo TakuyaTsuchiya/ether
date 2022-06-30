df6167311063ac9245d593b2dc5213be978b3f89
Takuya Tsuchiya <takuya21@gmail.com>
2022-06-30 14:23:42 +0900

    MetaMaskのアドレスを取得してalert

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 console.log("hello world!");
 console.log("こんにちは、Ether.jsの勉強をしています");
 
-async function myFunction() {
-  
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-  const blocknumber = await provider.getBlockNumber()
-  
+async function myFunction() {  
+  const provider = await new ethers.providers.Web3Provider(window.ethereum)
+  const blocknumber = await provider.getBlockNumber();
   alert(blocknumber);
 }  
+
+async function myFunction2() {  
+  const provider = await new ethers.providers.Web3Provider(window.ethereum)
+  const addresses = await ethereum.request({ method: 'eth_requestAccounts' });
+  const signer = await provider.getSigner();
+  alert(addresses[0]);
+}  
+
+window.onload = async function() {
+  myFunction();
+  myFunction2();
+}
 
 
